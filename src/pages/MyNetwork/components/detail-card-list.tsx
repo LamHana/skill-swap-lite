a07f89aaea80de1e8d7afbde1e1d7410e3724346
DaoHuongThao <daothao148@gmail.com>
2025-04-29 20:13:36 +0700

    fix: fetch connections

--- src/pages/MyNetwork/components/detail-card-list.tsx
+++ src/pages/MyNetwork/components/detail-card-list.tsx
@@ -3,19 +3,17 @@ import DetailCard from './detail-card';
 import useConnections from '@/hooks/useConnections';
 
 const DetailCardList = () => {
-  const { data: users = [], isLoading, isError } = useConnections();
+  const { data: users, isLoading, isError } = useConnections();
 
   return (
     <div className='flex flex-col items-start mt-10 w-full'>
       <h2 className='text-xl font-bold mb-4'>
         My Connections
-        <span className='font-medium'>{` (${users.length})`}</span>
+        <span className='font-medium'>{users ? ` (${users?.length})` : ` (${0})`}</span>
       </h2>
 
       <div className='flex flex-col gap-y-6 my-2 w-full'>
-        {users.map((user: User) => (
-          <DetailCard user={user} key={user.id} />
-        ))}
+        {users?.map((user: User) => <DetailCard user={user} key={user.id} />)}
       </div>
     </div>
   );
