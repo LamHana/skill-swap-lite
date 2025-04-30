3d29cc82482aff631e5f9fcda9d0b9303bb886b9
alicee-19 <anh2k4.neko@gmail.com>
2025-04-30 23:35:04 +0700

    ui: fix preview-card ui

--- src/components/common/preview-card.tsx
+++ src/components/common/preview-card.tsx
@@ -32,37 +32,46 @@ export default function PreviewCard({
   const { teachDisplay, learnDisplay } = getDisplayData();
 
   return (
-    <Card className={`w-full md:max-w-[400px] md:max-h-[315px] relative mx-auto overflow-hidden ${className}`} key={id}>
+    <Card
+      className={`w-full md:max-w-[280px] md:max-h-[240px] gap-3 relative mx-auto overflow-hidden ${className}`}
+      key={id}
+    >
       <div className='absolute top-0 right-0 bg-primary text-white font-bold py-2 px-4 rounded-tr-lg rounded-bl-lg'>
         {percent}%
       </div>
       <CardHeader>
         <div className='flex items-center gap-6'>
           <div className='rounded-lg'>
-            <Avatar className='w-[100px] h-[100px] !rounded-lg border-2 border-gray-200'>
+            <Avatar className='w-[70px] h-[70px] !rounded-lg border-2 border-gray-200'>
               <AvatarImage className='object-cover' src={photoUrl} alt={name} />
               <AvatarFallback className='!rounded-lg'>{name.charAt(0).toUpperCase()}</AvatarFallback>
             </Avatar>
           </div>
-          <CardTitle className='text-xl'>{name}</CardTitle>
+          <CardTitle className='text-lg'>{name}</CardTitle>
         </div>
       </CardHeader>
-      <CardContent className='flex flex-col gap-2'>
-        <div className='flex items-center gap-3 '>
-          <span className='font-bold text-lg'>Teaching</span>
-          <div className='flex gap-2'>
+      <CardContent className='flex flex-col gap-1'>
+        <div className='flex items-center gap-1 '>
+          <span className='font-bold text-sm'>Teaching</span>
+          <div className='flex gap-1'>
             {teachDisplay.map((item, index) => (
-              <span key={index} className={index !== 2 ? 'rounded-full px-3 py-1 border border-gray-300' : 'py-1'}>
+              <span
+                key={index}
+                className={index !== 2 ? 'rounded-full px-3 py-1 border border-gray-300 text-xs' : 'py-1 text-xs'}
+              >
                 {item}
               </span>
             ))}
           </div>
         </div>
-        <div className='flex items-center gap-3 mt-2'>
-          <span className='font-bold text-lg'>Learning</span>
-          <div className='flex gap-2'>
+        <div className='flex items-center gap-1 mt-2'>
+          <span className='font-bold text-sm'>Learning</span>
+          <div className='flex gap-1'>
             {learnDisplay.map((item, index) => (
-              <span key={index} className={index !== 2 ? 'rounded-full px-3 py-1 border border-gray-300' : 'py-1'}>
+              <span
+                key={index}
+                className={index !== 2 ? 'rounded-full px-3 py-1 border border-gray-300 text-xs' : 'py-1 text-xs'}
+              >
                 {item}
               </span>
             ))}
--- src/pages/Home/components/preview-card-list.tsx
+++ src/pages/Home/components/preview-card-list.tsx
@@ -53,7 +53,7 @@ const PreviewCardList = ({ results }: PreviewCardListProps) => {
 
   return (
     <div className='container mx-auto py-8'>
-      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8'>
         {results.map((result) => {
           const { mutate: connectMutate, status: connectStatus } = useMutation({
             mutationFn: () => {
@@ -92,6 +92,7 @@ const PreviewCardList = ({ results }: PreviewCardListProps) => {
                   {listPendingUsers[result.id] ? 'Pending' : 'Connect'}
                 </Button>
               }
+              className='mx-0'
             />
           );
         })}
