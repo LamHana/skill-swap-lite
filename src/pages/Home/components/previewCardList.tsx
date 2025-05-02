cf25650a114cdb87c0af541435cd437993a53278
alicee-19 <anh2k4.neko@gmail.com>
2025-05-02 18:00:56 +0700

    feat: search users

--- src/pages/Home/components/previewCardList.tsx
+++ src/pages/Home/components/previewCardList.tsx
@@ -65,7 +65,7 @@ const PreviewCardList = ({ results }: PreviewCardListProps) => {
             name={result.fullName.toString()}
             percent={result.percent}
             teach={Array.isArray(result.teach) ? result.teach : []}
-            learn={Array.isArray(result.teach) ? result.teach : []}
+            learn={Array.isArray(result.learn) ? result.learn : []}
             photoUrl={result.photoURL}
             button={
               <CustomButtonConnect
--- src/services/skill.service.ts
+++ src/services/skill.service.ts
@@ -1,10 +1,11 @@
 import { config } from '@/config/app';
-import { Skill } from '@/types/skill.type';
+import { CategoryWithSkills, Skill } from '@/types/skill.type';
 
 import { getDocs } from 'firebase/firestore';
 import { collection } from 'firebase/firestore';
 
 export const GET_SKILLS_QUERY_KEY = 'GET_SKILLS_QUERY_KEY';
+export const GET_SKILL_CATEGORIES_QUERY_KEY = 'GET_SKILL_CATEGORIES_QUERY_KEY';
 
 export const getSkills = async () => {
   const querySnapshot = await getDocs(collection(config.firebase.db, config.collections.skills));
@@ -16,4 +17,30 @@ export const getSkills = async () => {
     } as Skill;
   });
   return skills;
+};
+
+export const getAllCategories = async (): Promise<string[]> => {
+  const skills = await getSkills();
+  const uniqueCategories = new Set(skills.map((skill) => skill.category));
+  return Array.from(uniqueCategories);
+};
+
+export const getCategoriesWithSkills = async (): Promise<CategoryWithSkills[]> => {
+  const skills = await getSkills();
+  const categoriesMap = new Map<string, Omit<Skill, 'category'>[]>();
+
+  skills.forEach((skill) => {
+    if (!categoriesMap.has(skill.category)) {
+      categoriesMap.set(skill.category, []);
+    }
+    categoriesMap.get(skill.category)?.push({ id: skill.id, name: skill.name } as Omit<Skill, 'category'>);
+  });
+
+  return Array.from(categoriesMap.entries()).map(
+    ([category, skills]) =>
+      ({
+        category,
+        skills,
+      }) as CategoryWithSkills,
+  );
 };
