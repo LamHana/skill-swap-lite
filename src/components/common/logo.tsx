5d9f912ba357cef641940c9d8dc10b114015ac4c
Hanalam <lamtngochan@gmail.com>
2025-05-05 02:10:23 +0700

    fix: move logo to public

--- src/components/common/logo.tsx
+++ src/components/common/logo.tsx
@@ -3,7 +3,7 @@ import { config } from '@/config/app';
 const Logo = () => {
   return (
     <div className='flex items-center gap-2'>
-      <img src='/src/assets/images/logo.png' className='w-7' />
+      <img src='/public/logo.png' className='w-7' />
       <span className='font-semibold text-nowrap'>{config.name}</span>
     </div>
   );
